9b09f2947eba292bc7afba41258486838f71d3c5
Andrew Hansbury <dru@Andrews-MacBook-Pro.local>
2022-01-08 18:09:33 -0500

    menu buttons route to correct screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 
+import React, { useState } from 'react';
+
 
 
 function App() {
-  return (
-    <div className="App">
 
+	const[menuScreenState, setMenuScreenState] = useState(true)
+	const [createScreenState, setCreateScreenState]  = useState(false);
+	const [joinScreenState, setJoinScreenState]  = useState(false);
 
+	function handleJoinButtonClick(){
+		setJoinScreenState(true);
+		setMenuScreenState(false);
+
+	}
+
+	function handleCreateButtonClick () {
+	
+		setCreateScreenState(true);
+		setMenuScreenState(false);
+		  
+	}
+
+	function joinScreen(){
+		return (
+			<h1> Join Screen! </h1>
+		)
+	}
+
+	function createScreen(){
+		return (
+			<h1> Create Screen! </h1>
+		)
+	}
+	
+	function ShowMenu(){
+
+		
+		return (
+		
 		<div className="Menu-container">
 
 			<div className="M_header">
@@ -17,17 +50,34 @@ function App() {
 			</div>
 
 			<div className="M_join_button">
-			<button className="btn-hover color-7">Join!</button>
+				<button className="join-button btn-hover" onClick={() =>
+					{handleJoinButtonClick()}}>Join!</button>
 			</div>
 
 			<div className="M_join_button">
-			<button className="btn-hover color-8">Create Game</button>
+				<button className="create-button btn-hover" onClick={ () => 
+					{ handleCreateButtonClick()}}> Create Game </button>
 			</div>
 
 		</div>
+
+		);
+
+	}
+
+
+
+  return (
+    <div className="App">
+
+
+		{menuScreenState ? ShowMenu() : createScreenState ? createScreen() : joinScreenState ? joinScreen() : null } 
+		{/* {createScreenState ? createScreenState() : joinScreenState ? joinScreenState() : ShowMenu() }  */}
+				
+
+		{/* { createScreenState ? (<h1>DEEZNUTZ</h1>) : (ShowMenu())} */}
 		
-		
-					
+			
     </div>
   );
 }
