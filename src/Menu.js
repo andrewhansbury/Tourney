d82ef9f037b5fc01e808b206914544842dd3289c
andrewhansbury <73508007+andrewhansbury@users.noreply.github.com>
2022-04-29 00:10:47 -0400

    disabled autofocus for mobile and positioned menu buttons to bottom

--- src/Menu.js
+++ src/Menu.js
@@ -59,6 +59,24 @@ class Menu extends Component{
 		this.props.setMadeStates();
 	}
 
+	displayEntry(){
+		if (window.innerWidth> 480){
+			return (
+				<div className="M_code_entry">
+							<input value={this.game_code} autoFocus onChange={this.handleCodeInput} onKeyDown={(e) => this.enterPress(e)}  className = "entry-1" type="text" placeholder="6-DIGIT CODE" maxLength="6" />
+						</div>
+			);
+		}
+		else{
+			return (
+				<div className="M_code_entry">
+							<input value={this.game_code}  onChange={this.handleCodeInput} onKeyDown={(e) => this.enterPress(e)}  className = "entry-1" type="text" placeholder="6-DIGIT CODE" maxLength="6" />
+				</div>
+			);
+		}
+
+	}
+
 
     render() {
 
@@ -74,21 +92,17 @@ class Menu extends Component{
 						<h1>Tourney!</h1>
 					</div>
 
-					<div className="M_code_entry">
-						<input value={this.game_code} onChange={this.handleCodeInput} autoFocus onKeyDown={(e) => this.enterPress(e)}  className = "entry-1" type="text" placeholder="6-DIGIT CODE" maxLength="6" />
-					</div>
-
+					{this.displayEntry()}
+					
 					<div className="M_join_button">
 						<button className="join-button btn-hover" onClick={() =>
 							{this.handleJoinButtonClick()}} >Join!</button>
 					</div>
 
-					<div className="M_join_button">
+					<div className="M_bottom_container">
 						<button className="create-button btn-hover" onClick={ () => 
 							{this.handleCreateButtonClick()}}> Create Game </button>
-					</div>
-
-					<div> 
+				
 						<button className='create-buttons' onClick={ () => 
 							{this.handleMadeButtonClick()}}> I Already Made a Game</button>
 					</div>
